577b1eae3e2fd786ef767f90518ab4ef21f5a65d
Thomas <thomasmckenna12gmail.com>
2021-08-21 14:11:04 -0400

    commit tree addd1d027705e414ea41bf9151b46129c1d1896e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+
+// write a function that takes an array and a name in that array and returns all the
+// names that match the argument passed through. 
+
+
+  function findMatching(array, name) {
+    const newDrivers = array.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return newDrivers;
+
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(name => name.indexOf() === str.toLowerCase())
+  }
+
+
+
+
+// findMatching(drivers, "Bobby")
+
+
+
+
+
+// function findMatching(driver){
+//     return ["driver"] === 
+// }
+// 
+// findMatching();
+// 
+// function filter(collection) {
+//     for (const user of collection){
+//         if(findMatching(driver))
+//     }
+// }
+// 
+// drivers.filter(function(name, ){
+// 
+// })
